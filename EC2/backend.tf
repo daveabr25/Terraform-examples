7a9a5d9fc69570b553280f65c0556b0007f31951
daveabr25 <daveabr25@gmail.com>
2026-09-15 08:42:02 +0100

    Update backend.tf

--- EC2/backend.tf
+++ EC2/backend.tf
@@ -2,7 +2,7 @@ terraform {
 
 backend "s3" {
   
-       bucket = "devops-training-terraform-state-bucket12"
+       bucket = "devops-training-terraform-state-bucket1001"
        key = "dev/terraform.tfstate"
        region = "us-east-1"
        encrypt = true
